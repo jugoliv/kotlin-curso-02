82622477b8d48cafc257822e804daeeeb0ceb6bf
Juliano G. Oliveira <julianogomes.jar@gmail.com>
2022-10-19 23:04:00 -0300

    Modulo 01 - Passo 11: Criando diferentes tipos de funcionário : gerente (ctrl+c ctrl+v e particularidades próprias)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,12 +3,26 @@ fun main() {
 
     val alex = Funcionario(nome = "Alex",
             cpf = "111.111.111-11",
-            salario = 1000.0,
-            tipo = 0)
+            salario = 1000.0)
 
     println("nome ${alex.nome}")
     println("cpf ${alex.cpf}")
     println("salario ${alex.salario}")
-
     println("Bonificação ${alex.bonificacao()}")
+
+    val fran = Gerente(nome = "Fran",
+            cpf = "222.222.222-22",
+            salario = 2000.0,
+            senha = 1234)
+
+    println("\nnome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("Bonificação ${fran.bonificacao()}")
+
+    if(fran.autentica(1234)){
+        println("Autenticação com sucesso!")
+    } else {
+        println("*** Falha na autenticação ***")
+    }
 }
